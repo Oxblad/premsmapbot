aa3873e198d7beae0636bcefba587e822a6cbc7b
Oxblad <33175965+Oxblad@users.noreply.github.com>
2020-06-02 17:57:58 +0300

    Update index.py

--- index.py
+++ index.py
@@ -1675,8 +1675,7 @@ def handle_message_received(message):
             ff = dt.strftime("%Y")
             date = dt.strftime("%d.")
             time = dt.strftime("%H:%M")
-            date = int(date.replace('0', ''))
-            print(mouth[md])
+
             bot.send_message(chat_id,
                              f'Московское время: {time}\nДата:  {ff}, {mouth[md]}, {date}\nПользователей в боте: {len(premial) + 1}\nНомеров в процесcе: {len(running_spams_per_chat_id)}',
                              parse_mode='HTML')
